564deb0b01d03162372d5a5ebabeb2899ff03d1e
theploy67 <ploy.phu@student.mahidol.edu>
2025-10-07 19:41:39 +0700

    Fix init.sql/Fix pic product/ fix backend user

--- src/main/java/com/example/backend/config/CorsConfig.java
+++ src/main/java/com/example/backend/config/CorsConfig.java
@@ -1,8 +1,10 @@
-package com.example.backend;
+package com.example.backend.config;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
